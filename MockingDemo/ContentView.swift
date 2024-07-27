ce55ff940608ba2751459f9827a9c7af38cf88c4
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-07-27 17:20:43 +0530

    Initial Commit

---
+++ MockingDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MockingDemo
+//
+//  Created by Sajed Shaikh on 27/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MockingDemo/MockingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockingDemoApp.swift
+//  MockingDemo
+//
+//  Created by Sajed Shaikh on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MockingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
